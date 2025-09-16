5f33c774da64cbce53cdf4e10dcac7ca55a00b9e
yvessrib <yves@gec.inatel.br>
2025-09-16 16:37:06 -0300

    feat: experiences content

--- src/pages/experiences.tsx
+++ src/pages/experiences.tsx
@@ -12,6 +12,7 @@ export default function Experiences() {
 
   const sectionRef = useRef(null);
   const sectionRef2 = useRef(null);
+  const sectionRef3 = useRef(null);
 
   const { scrollYProgress: scrollYProgress1 } = useScroll({
     target: sectionRef,
@@ -20,6 +21,11 @@ export default function Experiences() {
 
   const { scrollYProgress: scrollYProgress2} = useScroll({
     target: sectionRef2,
+    offset: ["start center", "center start"],
+  });
+
+  const { scrollYProgress: scrollYProgress3} = useScroll({
+    target: sectionRef3,
     offset: ["start center", "center start"],
   });
 
@@ -42,22 +48,24 @@ export default function Experiences() {
 
         <div ref={sectionRef} className="flex flex-col w-full">
           <div  className="flex flex-col gap-4">
-            <span className="font-firacode font-semibold text-2xl text-violet-500 pt-2">Título - Lorem Ipsum</span>
+            <span className="font-firacode font-semibold text-2xl text-violet-500 pt-2">CP2eJr - Empresa Júnior </span>
 
             <div className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
-                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Cargo | Mês/Ano - Mês/Ano</span>
-                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam sem, blandit luctus leo ac, accumsan vulputate eros. Nullam at felis id mi iaculis tincidunt. Sed dignissim tellus in odio maximus, in luctus tellus pulvinar. Cras semper arcu cursus consequat malesuada. Sed a leo non mi tempor ornare. Curabitur luctus consequat laoreet.</span>
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Trainee de Projetos | 03/2022 - 06/2022</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">
+                  Como Trainee de Projetos, atuei em projetos de desenvolvimento web como parte da equipe técnica, com foco em front-end. Contribuí para a implementação de interfaces e recursos interativos, utilizando tecnologias como HTML, CSS e JavaScript, além de aplicar práticas de versionamento e metodologias ágeis como Git, GitHub, Scrum e Kanban.
+                </span>
               </div>
 
               <div className="flex flex-col gap-2">
-                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Cargo | Mês/Ano - Mês/Ano</span>
-                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam sem, blandit luctus leo ac, accumsan vulputate eros. Nullam at felis id mi iaculis tincidunt. Sed dignissim tellus in odio maximus, in luctus tellus pulvinar. Cras semper arcu cursus consequat malesuada. Sed a leo non mi tempor ornare. Curabitur luctus consequat laoreet.</span>
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Assessor de Projetos | 07/2022 - 12/2022</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei como Assessor de Desenvolvimento na CP2eJr, desempenhando funções de desenvolvedor front-end e gerente de projetos, onde criei sites utilizando HTML, CSS e JavaScript, e gerenciei projetos com ferramentas como Jira, GitHub e Pipefy, otimizando fluxos de trabalho e promovendo a colaboração eficaz da equipe de desenvolvimento</span>
               </div>
 
               <div className="flex flex-col gap-2">
-                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Cargo | Mês/Ano - Mês/Ano</span>
-                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam sem, blandit luctus leo ac, accumsan vulputate eros. Nullam at felis id mi iaculis tincidunt. Sed dignissim tellus in odio maximus, in luctus tellus pulvinar. Cras semper arcu cursus consequat malesuada. Sed a leo non mi tempor ornare. Curabitur luctus consequat laoreet.</span>
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Diretor de Projetos | 01/2023 - 12/2023</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei como Diretor de Desenvolvimento na CP2eJr, liderando a equipe de desenvolvimento web e supervisionando projetos para garantir eficiência e colaboração, utilizando ferramentas como Jira, Pipefy, Notion e GitHub Organizations. Mentorei membros da equipe para aprimorar habilidades técnicas, desenvolvi aplicações web com HTML, CSS, JavaScript, React.js e Node.js, e contribui para o planejamento estratégico trimestral da empresa, apoiando o alcance das metas organizacionais.</span>
               </div>
             </div>
           </div>
@@ -75,12 +83,45 @@ export default function Experiences() {
 
         <div ref={sectionRef2} className="flex flex-col w-full">
           <div  className="flex flex-col gap-4">
-            <span className="font-firacode font-semibold text-2xl text-violet-500 pt-2">Título - Lorem Ipsum</span>
+            <span className="font-firacode font-semibold text-2xl text-violet-500 pt-2">Amiko Soluções - Startup</span>
 
             <div className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
-                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Cargo | Mês/Ano - Mês/Ano</span>
-                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam sem, blandit luctus leo ac, accumsan vulputate eros. Nullam at felis id mi iaculis tincidunt. Sed dignissim tellus in odio maximus, in luctus tellus pulvinar. Cras semper arcu cursus consequat malesuada. Sed a leo non mi tempor ornare. Curabitur luctus consequat laoreet.</span>
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Estagiário Full-Stack | 07/2023 - Atualmente</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei no desenvolvimento de uma assistente virtual beira-leito para hospitais utilizando ReactJs, NextJs, NodeJs e AWS, além de reformular o site da empresa com NextJs e AWS, garantindo uma experiência mais otimizada e um site totalmente responsivo.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-6 mt-8">
+        <div className="flex flex-col align-middle justify-center items-center w-[50px] relative gap-2">
+          <Image src={StarImage} alt="" width={50} height={50} />
+          <motion.div
+            style={{ scaleY: scrollYProgress3 }}
+            className="h-full w-1 bg-violet-500 origin-top shadow-glow"
+          />
+        </div>
+
+        <div ref={sectionRef3} className="flex flex-col w-full">
+          <div  className="flex flex-col gap-4">
+            <span className="font-firacode font-semibold text-2xl text-violet-500 pt-2">Inatel - Instituto Nacional de Telecomunicações</span>
+
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Engenharia da Computação | 01/2021 - Atualmente</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei no desenvolvimento de uma assistente virtual beira-leito para hospitais utilizando ReactJs, NextJs, NodeJs e AWS, além de reformular o site da empresa com NextJs e AWS, garantindo uma experiência mais otimizada e um site totalmente responsivo.</span>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Monitor de Banco de Dados I | 08/2023 - 12/2024</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei como Monitor de Ensino na disciplina Banco de Dados I, planejando, desenvolvendo e ministrando aulas para alunos de engenharia, ensinando fundamentos de MySQL e conceitos de gerenciamento de banco de dados. Ofereci suporte a alunos e professores, auxiliando na aplicação de provas e avaliação de trabalhos, contribuindo para o aprendizado e desempenho acadêmico da turma.</span>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <span className="font-poppins font-semibold text-xl text-black dark:text-white">Programação 1.0 - Iniciação Científica | 02/2022 - 06/2023</span>
+                <span className="font-firacode font-semibold text-base text-zinc-800 dark:text-zinc-300">Atuei como Assistente de Ensino em Programação em Python, planejando, desenvolvendo e ministrando aulas para alunos do ensino médio, introduzindo-os à programação por meio de competições interescolares e criando conteúdos educacionais envolventes para estimular o pensamento lógico e habilidades de resolução de problemas.</span>
               </div>
             </div>
           </div>
