ba6a31477da7a20b3ddf0d4aab48605a55d55441
yvessrib <yves@gec.inatel.br>
2025-09-17 17:47:49 -0300

    feat: eslint rules config

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }),
+]
+
+export default eslintConfig
